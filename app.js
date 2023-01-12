45e16ab7fbf95436d0ce134b279dbd63a1beecef
email <racbernazgul@gmail.com>
2023-01-12 17:44:51 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+
+document.querySelector("body").addEventListener("mousemove", steve);
+
+function steve(e) {
+  document.querySelector(".container1").style.backgroundColor = `rgb(${
+    e.screenX % 255
+  }, ${e.screenY % 255}, ${(e.screenX + e.screenY) % 255})`;
+
+  document.querySelector(".container2").style.backgroundColor = `rgb(${
+    e.screenX % 200
+  }, ${e.screenY % 200}, ${(e.screenX + e.screenY) % 200})`;
+
+  document.querySelector(".container3").style.backgroundColor = `rgb(${
+    e.screenX % 150
+  }, ${e.screenY % 150}, ${(e.screenX + e.screenY) % 150})`;
+
+  document.querySelector(".container4").style.backgroundColor = `rgb(${
+    e.screenX % 100
+  }, ${e.screenY % 100}, ${(e.screenX + e.screenY) % 100})`;
+
+  document.querySelector("body").style.backgroundColor = `rgb(${
+    e.screenX % 50
+  }, ${e.screenY % 50}, ${(e.screenX + e.screenY) % 50})`;
+}
